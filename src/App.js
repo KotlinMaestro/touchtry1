e8f865ad1bdbf5a019bdddeb1f888434b999f12d
KotlinMaestro <servincentparsley@gmail.com>
2023-01-12 23:03:00 +0300

    redux shit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import EditUser from "./components/EditUser";
 import ShowUsers from "./components/ShowUsers";
-
+import store from '.redux/store'
 //store - to hold all the data/ the grand or global state
 
-//Actions -
+//Actions - FETCH_DATA, PATCH_DATA
+
+//Reducer - How actions describe state to the next state
+
+//Dispatch
 
 
 function App() {
